72998512c138c7d0fa8438b7309b5c24483bc261
Lior <lkashti@gmail.com>
2021-07-03 22:40:33 +0300

    Add initial router files

---
+++ server/index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const PORT = 3000;
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to database"));
+
+// Handling JSON
+app.use(express.json());
+app.use(express.urlencoded());
+
+// Cross-origin resource sharing
+app.use(cors());
+
+// Static folder and files
+// app.use(express.static("public"));
+
+// Locations router
+const blocksRouter = require("./routes/blocks");
+app.use("/blocks", blocksRouter);
+
+
+// Server start
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
