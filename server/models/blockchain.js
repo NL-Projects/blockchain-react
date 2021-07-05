6facbacfd83826070a557c33278b533ff8ed0a62
Lior Kashti <lkashti@gmail.com>
2021-07-05 20:56:35 +0300

    Added blockchain module and a key generator

---
+++ server/models/blockchain.js
@@ -0,0 +1,54 @@
+const SHA256 = require("crypto-js/sha256");
+class Block {
+  constructor(index, timestamp, transactions, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.previousHash = previousHash;
+    this.transactions = transactions;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.nonce +
+        this.timestamp +
+        JSON.stringify(this.transactions).toString()
+    );
+  }
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0 ")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block Mined: " + this.hash);
+  }
+}
+class BlockChain {
+  constructor(difficulty = 0) {
+    this.chain = [this.createGenesisBlock];
+    this.difficulty = difficulty;
+  }
+  createGenesisBlock() {
+    return new Block(0, "5/7/2021", "Genesis Block", "0");
+  }
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(block);
+  }
+}
+class Transaction {
+  constructor(fromAddress, toAddress, amount) {
+    this.fromAddress = fromAddress;
+    this.toAddress = toAddress;
+    this.amount = amount;
+  }
+}
+module.exports = { Block, BlockChain, Transaction };
