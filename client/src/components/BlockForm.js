d1f4204648c4bbd40c2a4515b6a380654ea3caca
Noamico123 <Noam.Mishaeli@gmail.com>
2021-07-19 00:58:53 +0300

    almost finished with block, needs to check about the mine function - it blows the program

--- client/src/components/BlockForm.js
+++ client/src/components/BlockForm.js
@@ -1,39 +1,123 @@
-import Button from "./Button";
 import "../index.css";
-const { BlockChain, Block } = require("../models/blockchain_functionallity");
+import Button from "./Button";
+import { useState } from "react";
+const SHA256 = require("crypto-js/sha256");
+// const { BlockChain, Block } = require("../services/blockchain_functionallity");
 
 function BlockForm() {
-  // const [blockChain, setBlockChain] = useState(new BlockChain());
-  // const [block, setBlock] = useState(blockChain.getLatestBlock());
-  // blockChain.addBlock(new Block(1, "20/01/2019", { amount: 4 }));
-  // blockChain.addBlock(new Block(2, "20/02/2019", { amount: 8 }));
+  const [block, setBlock] = useState({
+    index: 1,
+    timestamp: "",
+    nounce: 12,
+    data: "Hello world",
+    hash: SHA256(1, "", 12, JSON.stringify("Hello world")).toString(),
+    // transactions: "",
+    // previousHash: "",
+  });
+
+  const index = block.index;
+  const timestamp = block.timestamp;
+  const nounce = block.nounce;
+  const data = block.data;
+
+  console.log(block);
+
+  function calculateHash(index, timestamp, nounce, data) {
+    return SHA256(index + timestamp + nounce + JSON.stringify(data)).toString();
+  }
+
+  function setIndex(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+    setBlock((prevBlock) => ({
+      ...prevBlock,
+      [name]: value,
+      hash: calculateHash(index, timestamp, nounce, data),
+    }));
+  }
+
+  function setNounce(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+    setBlock((prevBlock) => ({
+      ...prevBlock,
+      [name]: value,
+      hash: calculateHash(index, timestamp, nounce, data),
+    }));
+  }
+
+  function setData(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+    setBlock((prevBlock) => ({
+      ...prevBlock,
+      [name]: value,
+      hash: calculateHash(index, timestamp, nounce, data),
+    }));
+  }
+
+  function handleSubmit(difficulty = 5) {
+    while (
+      block.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0 ")
+    ) {
+      block.nonce++;
+      block.hash = calculateHash(index, timestamp, nounce, data);
+    }
+    console.log("Block Mined: " + block.hash);
+  }
 
   return (
     <div>
-      <div>
-        Block:
-        <input className="inputBlockLength" type="text" placeholder="block" />
-      </div>
-      <div>
-        Nounce:
-        <input className="inputBlockLength" type="text" placeholder="" />
-      </div>
-      <div>
-        Data:
-        <form>
-          <textarea className="blockData" placeholder="" />
-          <span>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Block:
+          <input
+            className="inputBlockLength"
+            type="text"
+            name="index"
+            placeholder={index}
+            onChange={(e) => setIndex(e)}
+          />
+        </div>
+        <div>
+          Nounce:
+          <input
+            className="inputBlockLength"
+            type="text"
+            name="nounce"
+            placeholder=""
+            onChange={(e) => setNounce(e)}
+          />
+        </div>
+        <div>Data:</div>
+        <textarea
+          className="blockData"
+          placeholder=""
+          name="data"
+          onChange={(e) => setData(e)}
+        />
+        <div>
+          <div>
             Hash:
             <input
               className="inputBlockLength"
               type="text"
-              // placeholder={hashedData}
+              placeholder={block.hash}
               disabled
             />
-          </span>
-          <Button text="Mine" />
-        </form>
-      </div>
+          </div>
+          <div hidden>
+            Previous Block:
+            <input
+              className="inputBlockLength"
+              type="text"
+              placeholder={block.hash}
+              disabled
+            />
+          </div>
+          <Button text="Mine" type="submit" value="Submit" />
+        </div>
+      </form>
     </div>
   );
 }
--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,5 +1,9 @@
 import "../index.css";
-function Button({ text }) {
-  return <button className="btn">{text}</button>;
+function Button({ text, type, value }) {
+  return (
+    <button className="btn" type={type} value={value}>
+      {text}
+    </button>
+  );
 }
 export default Button;
