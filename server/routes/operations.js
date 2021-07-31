3eafbbda665043c94f87d1719ecfc0883bc58256
Noamico123 <Noam.Mishaeli@gmail.com>
2021-07-31 16:26:28 +0300

    tobedeleted

--- server/routes/operations.js
+++ server/routes/operations.js
@@ -12,16 +12,45 @@ router.post("/sha256", (req, res) => {
 
 router.post("/mine", (req, res) => {
   req = req.body;
-  let newBlock = new Block(
-    req.index,
-    req.data,
-    req.previousHash
-  );
+  let newBlock = new Block(req.index, req.data, req.previousHash);
   newBlock.mineBlock(4);
   res.json({
-      nonce:newBlock.nonce,
-      hash:newBlock.hash.toString()
-    });
+    nonce: newBlock.nonce,
+    hash: newBlock.hash.toString(),
+  });
+});
+
+router.post("/hashBlock", (req, res) => {
+  req = req.body;
+  let newBlock = new Block(
+    req.data.blockData.idx,
+    req.data.blockData.transactions,
+    req.data.blockData.previousHash
+  );
+  newBlock.calculateHash();
+  res.json({
+    index: newBlock.index,
+    nonce: newBlock.nonce,
+    hash: newBlock.hash,
+  });
+});
+
+router.post("/genesisBlock", (req, res) => {
+  req = req.body;
+  var blockChain = new BlockChain(req.difficulty);
+  blockChain.createGenesisBlock();
+});
+
+router.post("/add", (req, res) => {
+  req = req.body;
+  console.log(req.data);
+  // let addBlock = new BlockChain(req.difficulty);
+  // addBlock.addBlock(req);
+  res.json({
+    // index: newBlock.index,
+    // nonce: addBlock.nonce,
+    // hash: addBlock.hash.toString(),
+  });
 });
 
 module.exports = router;
